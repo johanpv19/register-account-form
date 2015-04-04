c989093f702d2b277244bb357e89c324d84c963a
Johan <johanpv19@gmail.com>
2015-04-03 20:22:00 -0400

    . Adding files from version 0.1

---
+++ app/nls/es/strings.js
@@ -0,0 +1,24 @@
+﻿// nls/es
+
+define({
+    title: "Registrar",
+    header: "Registrar - Crear Cuenta",
+    requiredfieldsmessagestart: "Los campos marcados con",
+    requiredfieldsmessagesend: "son campos abligatorios",
+    yourname: "Tu nombre",
+    emailid: "ID de E-Mail",
+    alternateemailid: "ID de E-Mail Alternativo",
+    yourphone: "Tu teléfono",
+    countrycode: "Código del País",
+    citycode: "Código de la Ciudad",
+    phonenumber: "Número de Teléfono",
+    mobilephone: "Teléfono Móvil",
+    yourpostaladdress: "Tu Dirección Postal",
+    country: "País",
+    password: "Contraseña",
+    passwordconfirm: "Repetir Contraseña",
+    termsaccept: "Acepto los",
+    termslink: "Términos de Uso",
+    continue: "Continuar >>",
+    reset: "Borrar"
+});
